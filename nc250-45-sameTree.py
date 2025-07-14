aa9128f90b78d7b740c6914af5427fd12c93bb13
Hariharasudan S A <66832228+Hari3008@users.noreply.github.com>
2025-07-14 18:16:43 -0400

    45. Same Tree
    Check the comments for the logic

---
+++ nc250-45-sameTree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+# First check if the node exists, if one of the node exists but the other node doesn't exist then return false. check for the values too. Traverse through the left and right subtrees 
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
