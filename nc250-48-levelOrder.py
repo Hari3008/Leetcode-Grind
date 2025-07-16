0ac1837514b9ee6a53d5574540b984eecf959ce7
Hariharasudan S A <66832228+Hari3008@users.noreply.github.com>
2025-07-15 22:25:47 -0400

    Create nc250-48-levelOrder.py
    Check comments -> BFS clearly

---
+++ nc250-48-levelOrder.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        # Okay from the look of the question it is obvious that we need to do BFS
+        # So first add the element and then its childen. for every level add the element
+        res = []
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            qLen = len(q)
+            level = []
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    q.append(node.left)
+                    q.append(node.right)
+                    level.append(node.val)
+            if level:
+                res.append(level)
+        
+        return res
+        
